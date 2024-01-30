86166c304436ca29f21f673c1a0580a256afa6f4
Yasmine Aoua <yfaoua@wpi.edu>
2024-01-29 20:54:08 -0500

    Added mech 2d sim to the shooter angle file, but it was not working

--- competition/2024 KwarQs competition code/src/main/java/frc/robot/subsystems/shooter/ShooterAngle.java
+++ competition/2024 KwarQs competition code/src/main/java/frc/robot/subsystems/shooter/ShooterAngle.java
@@ -7,15 +7,26 @@ package frc.robot.subsystems.shooter;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.RobotController;
 //import frc.robot.Constants.ArmConstants;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 import frc.robot.devices.NeoMotor;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 
 /** A robot arm subsystem that moves with a motion profile. */
 public class ShooterAngle extends ProfiledPIDSubsystem {
@@ -29,12 +40,20 @@ public class ShooterAngle extends ProfiledPIDSubsystem {
   public static final double kP = 0.1;
   public static final int kEncoderPPR = 256;
   public static final double kEncoderDistancePerPulse = 2.0 * Math.PI / kEncoderPPR;
+  private MechanismLigament2d pivot;
+  private double pivotMotorPercent = 0;
+  private Rotation2d pivotAngle = new Rotation2d(0);
+  private final FlywheelSim pivotSimMotor = new FlywheelSim(DCMotor.getNEO(1), 6.75, 0.025);
+
 
   public static final double kArmOffsetRads = 0.5;
   private final NeoMotor shooter_angle_motor;
   private final ArmFeedforward m_feedforward = new ArmFeedforward(
       kSVolts, kGVolts,
       kVVoltSecondPerRad, kAVoltSecondSquaredPerRad);
+
+      
+
 
   /** Create a new ArmSubsystem. */
   public ShooterAngle() {
@@ -51,12 +70,30 @@ public class ShooterAngle extends ProfiledPIDSubsystem {
     shooter_angle_motor.setConversionFactor(kEncoderDistancePerPulse);
     // Start arm at rest in neutral position
     setGoal(kArmOffsetRads);
+
+     Mechanism2d mech = new Mechanism2d(3, 3);
+        // the mechanism root node
+        MechanismRoot2d arm = mech.getRoot("arm", 1.5, 0.5);
+        pivot = arm.append(
+            new MechanismLigament2d("pivot", Units.inchesToMeters(21), 0, 10, new Color8Bit(Color.kOrange)));
+        SmartDashboard.putData("Mech2d", mech);
+        pivotSimMotor.setInput(0);
   }
   public void setAngle(double degrees){
     var rotation = new Rotation2d(degrees);
     setGoal(rotation.getRadians());
   }
 
+  @Override
+  public void simulationPeriodic(){
+    super.simulationPeriodic();
+    pivotSimMotor.setInputVoltage(pivotMotorPercent * RobotController.getBatteryVoltage());
+    pivotSimMotor.update(.02);
+    var encoderRateSign = 1;
+    var pivotRate = pivotSimMotor.getAngularVelocityRadPerSec() * encoderRateSign;
+    pivotAngle = pivotAngle.plus(Rotation2d.fromRadians(pivotRate * 0.02));
+    pivot.setAngle(pivotAngle.getDegrees() + 90);
+  }
   @Override
   public void useOutput(double output, TrapezoidProfile.State setpoint) {
     // Calculate the feedforward from the sepoint
