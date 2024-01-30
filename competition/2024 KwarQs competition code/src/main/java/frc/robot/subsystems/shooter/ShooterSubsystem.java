2d0abaab7d5842887054ab1975828b5a566533ad
krimo <kaoua@sarah-olkens-MacBook-Pro-2.local>
2024-01-29 19:40:14 -0500

    Removed the shooter angle code from the shootersubsytem and identified what the functions do

--- competition/2024 KwarQs competition code/src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ competition/2024 KwarQs competition code/src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -26,60 +26,37 @@ public class ShooterSubsystem extends SubsystemBase {
 
     private NeoMotor shooterMotorOne;
     private NeoMotor shooterMotorTwo;
-    private NeoMotor shooterAngleMotor;
-    private Rotation2d shooterAngle = new Rotation2d();
     private double shooterSpeed = 0;
-    private CANcoder angleEncoder;
-    private double shooterAngleVoltage;
-    public static final double MAX_SHOOTER_ANGLE_VOLTAGE = 4;
-    private ProfiledPIDController shooterPID = new ProfiledPIDController((Robot.isSimulation()) ? .001 : .005, 0, 0, new TrapezoidProfile.Constraints(360, 420));
     
     
-    public ShooterSubsystem()
-    {
-        
+    public ShooterSubsystem(){
         shooterMotorOne = new NeoMotor(28); // Correct these when we know the numbers
         shooterMotorTwo = new NeoMotor(35);
-        shooterAngleMotor = new NeoMotor(37);
         shooterMotorTwo.setInverted(true);
         shooterMotorOne.setFollower(shooterMotorTwo);
-        angleEncoder= new CANcoder(8);
     }
+
+    //Shooter turns on/ shoots the note
     public void shooterOn() {
         shooterMotorOne.setSpeed(shooterSpeed);
     }
+
+    //stops the shooter
     public void shooterOff() {
          shooterMotorOne.setSpeed(0);
     }
 
+    //Gets the current speed of the shooter
     public double getSpeed() {
         return shooterMotorOne.getSpeed();
     }
 
-    public double getAngle() {
-      return shooterAngle.getDegrees();
-    }
-
+    //The goal speed for the shooter
     public void setSpeed(double speed) {
         shooterSpeed = speed;
     }
-
-    private void setShooterAngleVoltage(double voltage) {
-        shooterAngleVoltage = voltage;
-    }
-     public void setAngle(double angle) {
-        double voltage = calculatePid(new Rotation2d(angle));
-    }
-
-    private double calculatePid(Rotation2d angle) {
-        return shooterPID.calculate(shooterAngle.getDegrees(), angle.getDegrees());
-    }
-
+    
     public void periodic() {
-        shooterAngle = Rotation2d.fromDegrees(angleEncoder.getAbsolutePosition().getValue());
-          var voltage = MathUtil.clamp(shooterAngleVoltage, -MAX_SHOOTER_ANGLE_VOLTAGE, MAX_SHOOTER_ANGLE_VOLTAGE);
-          var shooterAngleMotorPercent = (voltage / RobotController.getBatteryVoltage());
-          shooterAngleMotor.setSpeed(shooterAngleMotorPercent);
     }
 
 }
