73d4610a1a116122bbf072c427d1388a6b99e42d
amorygalili <amorygalili@gmail.com>
2024-02-06 17:15:30 -0500

    wip

--- competition/2024 KwarQs competition code/src/main/java/frc/robot/RobotContainer.java
+++ competition/2024 KwarQs competition code/src/main/java/frc/robot/RobotContainer.java
@@ -24,37 +24,31 @@ import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 import java.io.File;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very
- * little robot logic should actually be handled in the {@link Robot} periodic methods (other than the scheduler calls).
- * Instead, the structure of the robot (including subsystems, commands, and trigger mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very
+ * little robot logic should actually be handled in the {@link Robot} periodic
+ * methods (other than the scheduler calls).
+ * Instead, the structure of the robot (including subsystems, commands, and
+ * trigger mappings) should be declared here.
  */
 
 public class RobotContainer {
-  String deployDirectory = (Robot.isSimulation())? "neo" : "swerve";
+  String deployDirectory = (Robot.isSimulation()) ? "neo" : "swerve";
   // The robot's subsystems and commands are defined here...
   private final SwerveSubsystem drivebase = new SwerveSubsystem(
-    new File(Filesystem.getDeployDirectory(), deployDirectory)
-  );
+      new File(Filesystem.getDeployDirectory(), deployDirectory));
 
   private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(2);
   private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(2);
   private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(2);
 
-<<<<<<< HEAD
-    // A chooser for autonomous commands
-    SendableChooser<String> m_chooser = new SendableChooser<>();
+  // A chooser for autonomous commands
+  SendableChooser<String> m_chooser = new SendableChooser<>();
 
-
-    
-  // CommandJoystick rotationController = new CommandJoystick(1);
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  CommandJoystick driverController = new CommandJoystick(1);
-
-  // CommandJoystick driverController   = new CommandJoystick(3);//(OperatorConstants.DRIVER_CONTROLLER_PORT)
-=======
->>>>>>> main
+  // CommandJoystick driverController = new
+  // CommandJoystick(3);//(OperatorConstants.DRIVER_CONTROLLER_PORT)
   XboxController driverXbox = new XboxController(0);
-  IntakeSubsystem intake =new IntakeSubsystem();
+  IntakeSubsystem intake = new IntakeSubsystem();
   ShooterSubsystem shooter = new ShooterSubsystem();
 
   /**
@@ -64,131 +58,58 @@ public class RobotContainer {
     // Configure the trigger bindings
     configureBindings();
     intake.beltStop();
-    SmartDashboard.putData("Intake",intake);
+    SmartDashboard.putData("Intake", intake);
     SmartDashboard.putData("SwerveSubsystem", drivebase);
 
-<<<<<<< HEAD
     // Add commands to the autonomous command chooser
     m_chooser.setDefaultOption("Taxi Auto", "Taxi Auto");
     m_chooser.addOption("Amp to Note Auto", "Amp to Note Auto");
-     m_chooser.addOption("Yo Auto", "Yo Auto");
+    m_chooser.addOption("Yo Auto", "Yo Auto");
     m_chooser.addOption("YoYo Auto", "YoYo Auto");
 
     // Put the chooser on the dashboard
     Shuffleboard.getTab("Autonomous").add(m_chooser);
-    AbsoluteDriveAdv closedAbsoluteDriveAdv = new AbsoluteDriveAdv(
-      drivebase,
-      () -> {
-        double y = MathUtil.applyDeadband(
-          driverXbox.getLeftY(),
-          OperatorConstants.LEFT_Y_DEADBAND
-        );
-        return m_yspeedLimiter.calculate(y);
-      },
-      () -> {
-        double x = MathUtil.applyDeadband(
-          driverXbox.getLeftX(),
-          OperatorConstants.LEFT_X_DEADBAND
-        );
-        return m_xspeedLimiter.calculate(x);
-      },
-      () -> {
-        double rot = MathUtil.applyDeadband(
-          driverXbox.getRightX(),
-          OperatorConstants.RIGHT_X_DEADBAND
-        );
-          return m_rotLimiter.calculate(rot);
-      },
-      driverXbox::getYButtonPressed,
-      driverXbox::getAButtonPressed,
-      driverXbox::getXButtonPressed,
-      driverXbox::getBButtonPressed
-    );
 
-    // Applies deadbands and inverts controls because joysticks
-    // are back-right positive while robot
-    // controls are front-left positive
-    // left stick controls translation
-    // right stick controls the desired angle NOT angular rotation
-    Command driveFieldOrientedDirectAngle = drivebase.driveCommand(
-      () -> {
-        double y = MathUtil.applyDeadband(
-          driverXbox.getLeftY(),
-          OperatorConstants.LEFT_Y_DEADBAND
-        );
-          return m_yspeedLimiter.calculate(y);
-      },
-
-      () -> {
-        double x = MathUtil.applyDeadband(
-          driverXbox.getLeftX(),
-          OperatorConstants.LEFT_X_DEADBAND
-        );
-          return m_xspeedLimiter.calculate(x);
-      },
-      () -> driverXbox.getRightX(),
-      () -> driverXbox.getRightY()
-    );
-
-    
-
-    // Applies deadbands and inverts controls because joysticks
-    // are back-right positive while robot
-    // controls are front-left positive
-    // left stick controls translation
-    // right stick controls the angular velocity of the robot
-=======
->>>>>>> main
     Command driveFieldOrientedAnglularVelocity = drivebase.driveCommand(
-      () -> {
-        double y = MathUtil.applyDeadband(
-          -driverXbox.getLeftY(),
-          //  Math.copySign(Math.pow(-driverXbox.getLeftY(), 2), -driverXbox.getLeftY()),
-          OperatorConstants.LEFT_Y_DEADBAND
-        );
-        return m_yspeedLimiter.calculate(y);
-      },
-      () -> {
-        double x = MathUtil.applyDeadband(
-          -driverXbox.getLeftX(),
-          // Math.copySign(Math.pow(-driverXbox.getLeftX(), 2), -driverXbox.getLeftX()),
-          OperatorConstants.LEFT_X_DEADBAND
-        );
-        return m_xspeedLimiter.calculate(x);
-      },
-      () -> -driverXbox.getRightX()
-    );
-
-   
-
+        () -> {
+          double y = MathUtil.applyDeadband(
+              -driverXbox.getLeftY(),
+              // Math.copySign(Math.pow(-driverXbox.getLeftY(), 2), -driverXbox.getLeftY()),
+              OperatorConstants.LEFT_Y_DEADBAND);
+          return m_yspeedLimiter.calculate(y);
+        },
+        () -> {
+          double x = MathUtil.applyDeadband(
+              -driverXbox.getLeftX(),
+              // Math.copySign(Math.pow(-driverXbox.getLeftX(), 2), -driverXbox.getLeftX()),
+              OperatorConstants.LEFT_X_DEADBAND);
+          return m_xspeedLimiter.calculate(x);
+        },
+        () -> -driverXbox.getRightX());
 
     Command driveFieldOrientedDirectAngleSim = drivebase.simDriveCommand(
-      () -> {
-        double y = MathUtil.applyDeadband(
-          -driverXbox.getLeftY(),
-          OperatorConstants.LEFT_Y_DEADBAND
-        );
-        return m_yspeedLimiter.calculate(y);
-      },
-      () -> {
-        double x = MathUtil.applyDeadband(
-          driverXbox.getLeftX(),
-          OperatorConstants.LEFT_X_DEADBAND
-        );
-        return m_xspeedLimiter.calculate(x);
-      },
-      () -> driverXbox.getRawAxis(2)
-    );
+        () -> {
+          double y = MathUtil.applyDeadband(
+              -driverXbox.getLeftY(),
+              OperatorConstants.LEFT_Y_DEADBAND);
+          return m_yspeedLimiter.calculate(y);
+        },
+        () -> {
+          double x = MathUtil.applyDeadband(
+              driverXbox.getLeftX(),
+              OperatorConstants.LEFT_X_DEADBAND);
+          return m_xspeedLimiter.calculate(x);
+        },
+        () -> driverXbox.getRawAxis(2));
 
     drivebase.setDefaultCommand(
-      !RobotBase.isSimulation()
-        ? driveFieldOrientedAnglularVelocity
-        : driveFieldOrientedDirectAngleSim
-    );
+        !RobotBase.isSimulation()
+            ? driveFieldOrientedAnglularVelocity
+            : driveFieldOrientedDirectAngleSim);
 
-
-    //auto commands
-      //EXAMPLE:  NamedCommands.registerCommand("useless", exampleSubsystem.exampleCommand());
+    // auto commands
+    // EXAMPLE: NamedCommands.registerCommand("useless",
+    // exampleSubsystem.exampleCommand());
 
   }
 
@@ -196,17 +117,23 @@ public class RobotContainer {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
 
     new JoystickButton(driverXbox, 1)
-      .onTrue((new InstantCommand(drivebase::zeroGyro)));
-    //new JoystickButton(driverXbox, 3)
-     // .onTrue(new InstantCommand(drivebase::addFakeVisionReading));
-    //    new JoystickButton(driverXbox, 3).whileTrue(new RepeatCommand(new InstantCommand(drivebase::lock, drivebase)));
+        .onTrue((new InstantCommand(drivebase::zeroGyro)));
+    // new JoystickButton(driverXbox, 3)
+    // .onTrue(new InstantCommand(drivebase::addFakeVisionReading));
+    // new JoystickButton(driverXbox, 3).whileTrue(new RepeatCommand(new
+    // InstantCommand(drivebase::lock, drivebase)));
 
-   new JoystickButton(driverXbox, XboxController.Button.kY.value).whileTrue(new RunCommand(intake::intake)).onFalse(new RunCommand(intake::beltStop));;
-   new JoystickButton(driverXbox, XboxController.Button.kB.value).whileTrue(new RunCommand(intake::outtake)).onFalse(new RunCommand(intake::beltStop));;
-   new JoystickButton(driverXbox, XboxController.Button.kA.value).onTrue(new RunCommand(intake::extend));
-   new JoystickButton(driverXbox, XboxController.Button.kX.value).onTrue(new RunCommand(intake::retract));
-   Command shooterCommand = shooter.revAndShoot(); 
-   new Trigger(() -> driverXbox.getRightTriggerAxis() > .5).onTrue(shooterCommand).onFalse(new RunCommand(() -> shooterCommand.cancel()));
+    new JoystickButton(driverXbox, XboxController.Button.kY.value).whileTrue(new RunCommand(intake::intake))
+        .onFalse(new RunCommand(intake::beltStop));
+    ;
+    new JoystickButton(driverXbox, XboxController.Button.kB.value).whileTrue(new RunCommand(intake::outtake))
+        .onFalse(new RunCommand(intake::beltStop));
+    ;
+    new JoystickButton(driverXbox, XboxController.Button.kA.value).onTrue(new RunCommand(intake::extend));
+    new JoystickButton(driverXbox, XboxController.Button.kX.value).onTrue(new RunCommand(intake::retract));
+    Command shooterCommand = shooter.revAndShoot();
+    new Trigger(() -> driverXbox.getRightTriggerAxis() > .5).onTrue(shooterCommand)
+        .onFalse(new RunCommand(() -> shooterCommand.cancel()));
   }
 
   /**
@@ -220,7 +147,7 @@ public class RobotContainer {
   }
 
   public void setDriveMode() {
-    //drivebase.setDefaultCommand();
+    // drivebase.setDefaultCommand();
   }
 
   public void setMotorBrake(boolean brake) {
