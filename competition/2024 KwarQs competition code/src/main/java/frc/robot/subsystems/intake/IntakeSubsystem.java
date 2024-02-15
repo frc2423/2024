abc567a852786dd8613a183f9ee13bcec6ab0b83
amorygalili <amorygalili@gmail.com>
2024-02-15 17:25:18 -0500

    Tuned shooter angle for new gearing

--- competition/2024 KwarQs competition code/src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ competition/2024 KwarQs competition code/src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -170,7 +170,6 @@ public class IntakeSubsystem extends SubsystemBase {
         double maxSpeed = .5;
         pivotMotorPercent = Math.max(Math.min(maxSpeed, pivotMotorPercent), -maxSpeed);
 
-        // m_Pivot.set(pivotMotorPercent);
         m_Pivot.set(pivotMotorPercent);
 
         // double m_Pivot_Pos = getMeasurement();
