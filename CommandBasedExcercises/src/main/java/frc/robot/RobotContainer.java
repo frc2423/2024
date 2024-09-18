783bd5e31c095c472ab48913aa0692eccd1c11d9
amorygalili <amorygalili@gmail.com>
2024-09-17 20:20:47 -0400

    wip

---
+++ CommandBasedExcercises/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.subsystems.LED.KwarqsLed;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class RobotContainer {
+  private final KwarqsLed led = new KwarqsLed();
+
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+    SmartDashboard.putData(led.disable());
+    SmartDashboard.putData(led.setOrange());
+    SmartDashboard.putData(led.setFirst3Green());
+    SmartDashboard.putData(led.setAllGreen());
+    SmartDashboard.putData(led.setEvenOdd());
+    SmartDashboard.putData(led.setBlackToGreenFade());
+    SmartDashboard.putData(led.setRainbow());
+    SmartDashboard.putData(led.setBouncy());
+  }
+
+  private void configureBindings() {
+   
+  }
+
+  public Command getAutonomousCommand() {
+    return Commands.none();
+  }
+}
