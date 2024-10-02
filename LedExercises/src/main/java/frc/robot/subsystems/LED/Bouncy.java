8764c3ecac294e97a0a5412459bc1c1af9a9fbe2
amorygalili <amorygalili@gmail.com>
2024-10-01 20:44:06 -0400

    made bouncy Led Rainbow BOomBOom

--- LedExercises/src/main/java/frc/robot/subsystems/LED/Bouncy.java
+++ LedExercises/src/main/java/frc/robot/subsystems/LED/Bouncy.java
@@ -6,22 +6,34 @@ import edu.wpi.first.wpilibj.AddressableLEDBuffer;
  * Make 10 LEDs bounce back and forth from start to end
  */
 public class Bouncy implements Led {
-    int coloredLedRange = 0;
+    int coloredLedRange = -1;
+    boolean reverse = false;
+    int currentHue = 0;
     public void start(AddressableLEDBuffer buffer, int length) {
     }
 
     public void run(AddressableLEDBuffer buffer, int length) {
         for (var i = 0; i < buffer.getLength(); i++) {
-            if (length< coloredLedRange + 9) {
-                if (i> coloredLedRange) {
-                    buffer.setRGB(i, 0, 0,255);
-                }
+            if (i> coloredLedRange && i<coloredLedRange+9) {
+                buffer.setHSV(i, currentHue, 255, 255);
             }
-            else {
-                buffer.setRGB(i, 0, 0, 0);
+            else{
+                buffer.setHSV(i, 0, 0, 0);
             }
         }
-        coloredLedRange+=1;
+        currentHue+=1;
+        if (reverse==false && coloredLedRange<length) {
+            coloredLedRange+=1;
+        }
+        if(reverse==true && coloredLedRange>-1) {
+            coloredLedRange-=1;
+        }
+        if(coloredLedRange==-1) {
+            reverse=false;
+        }
+        if(coloredLedRange==60) {
+            reverse=true;
+        }
     }
 
     public void end(AddressableLEDBuffer buffer, int length) {
