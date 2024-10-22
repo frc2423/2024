b4d72d7af0f3c2c184e684b89d12de1bb558deb0
amory-kwarqs <amory.kwarqs@gmail.com>
2024-10-21 20:56:14 -0400

    Hello, we tuned das and multiplied a random number by two. BYYYEEEE :P

--- competition/2024 KwarQs competition code/src/main/java/frc/robot/DAS.java
+++ competition/2024 KwarQs competition code/src/main/java/frc/robot/DAS.java
@@ -32,13 +32,15 @@ public class DAS {
 
     private void initializeMap() {
         // Example values, replace these with your actual mappings
-        distanceMap.put(/*distance*/1.32, new MotorSettings(/*angle*/323, -5200));//0 in
+        //distanceMap.put(/*distance*/1.23, new MotorSettings(/*angle*/321, -5200));
+        distanceMap.put(/*distance*/1.32, new MotorSettings(/*angle*/321.5, -5200));
         distanceMap.put(/*distance*/1.6, new MotorSettings(/*angle*/317, -5200)); //~11 in
-        distanceMap.put(/*distance*/1.71, new MotorSettings(/*angle*/316, -5200));//~15.5 in
-        distanceMap.put(/*distance*/2.0, new MotorSettings(/*angle*/313, -5200));//28
-        distanceMap.put(/*distance*/2.21, new MotorSettings(/*angle*/310, -5200));//32
-        distanceMap.put(/*distance*/2.4, new MotorSettings(/*angle*/308, -5200)); //40
-        distanceMap.put(/*distance*/2.7, new MotorSettings(/*angle*/305, -5200)); //50
+        distanceMap.put(/*distance*/1.71, new MotorSettings(/*angle*/315, -5200));//~15.5 in
+        distanceMap.put(/*distance*/1.9, new MotorSettings(/*angle*/311, -5200));
+        distanceMap.put(/*distance*/2.0, new MotorSettings(/*angle*/309, -5200));//28
+        distanceMap.put(/*distance*/2.21, new MotorSettings(/*angle*/307, -5200));//32
+        distanceMap.put(/*distance*/2.4, new MotorSettings(/*angle*/303.5, -5200)); //40
+        distanceMap.put(/*distance*/2.7, new MotorSettings(/*angle*/397.5, -5200)); //50
         distanceMap.put(/*distance*/3.01, new MotorSettings(/*angle*/300, -5200));//65in
         distanceMap.put(/*distance*/3.15, new MotorSettings(/*angle*/297, -5200));//70in
         distanceMap.put(/*distance*/3.502, new MotorSettings(/*angle*/293, -5200));//84
